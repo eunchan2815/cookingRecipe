38396c328fd16c90665ac470434540b9af7c75b4
eunchan2815 <164847037+eunchan2815@users.noreply.github.com>
2025-01-27 23:10:20 +0900

    🎉 - 프로젝트 시작

---
+++ cookingRecipe/cookingRecipe/Source/ApiKey.swift
@@ -0,0 +1,10 @@
+//
+//  ApiKey.swift
+//  cookingRecipe
+//
+//  Created by dgsw30 on 1/27/25.
+//
+
+import Foundation
+
+let apiKey = "c3440255d8684577b5b0"
---
+++ cookingRecipe/cookingRecipe/Source/cookingRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookingRecipeApp.swift
+//  cookingRecipe
+//
+//  Created by dgsw30 on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct cookingRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
